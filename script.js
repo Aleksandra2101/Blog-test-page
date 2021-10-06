8a4e8965aed5a21be91ae1a1d6b9b85fe1672d32
Aleksandra Stojanovska <alexandra.stojanovska2101@gmail.com>
2021-10-06 10:28:37 +0200

    final changes

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-fetch('http://localhost:3000/posts?_page=1&_limit=3')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    data.forEach(posts => {
+fetch("http://localhost:3000/posts?_page=1&_limit=3")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((posts) => {
       document.getElementById("cards").innerHTML += `
       <div class="col-md-4 mb-5">
       <div class="card w-100" >
@@ -16,26 +15,24 @@ fetch('http://localhost:3000/posts?_page=1&_limit=3')
           </div>
         </div>
   </div>
-  `
-    })
+  `;
+    });
   })
-  .catch(err => {
-    console.log(err)
-  })
-
+  .catch((err) => {
+    console.log(err);
+  });
 
 let pageNumber = 1;
 
 function loadPage(direction) {
-
-  if (direction === 'next') {
+  if (direction === "next") {
     pageNumber++;
     fetch(`http://localhost:3000/posts?_page=${pageNumber}&_limit=3`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        document.getElementById("cards").innerHTML = ""
-        data.forEach(posts => {
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        document.getElementById("cards").innerHTML = "";
+        data.forEach((posts) => {
           document.getElementById("cards").innerHTML += `
       <div class="col-md-4 mb-5">
       <div class="card w-100" >
@@ -48,18 +45,18 @@ function loadPage(direction) {
           </div>
         </div>
   </div>
-  `
-        })
-      })
-  } else if(direction === 'previous' && pageNumber > 1) {
+  `;
+        });
+      });
+  } else if (direction === "previous" && pageNumber > 1) {
     pageNumber--;
     fetch(`http://localhost:3000/posts?_page=${pageNumber}&_limit=3 `)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      document.getElementById("cards").innerHTML = ""
-      data.forEach(posts => {
-        document.getElementById("cards").innerHTML += `
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        document.getElementById("cards").innerHTML = "";
+        data.forEach((posts) => {
+          document.getElementById("cards").innerHTML += `
     <div class="col-md-4 mb-5">
     <div class="card w-100" >
         <div style="background-image:url(${posts.imageUrl}) ;height:250px;background-position:center"></div>
@@ -71,18 +68,12 @@ function loadPage(direction) {
         </div>
       </div>
 </div>
-`
-      })
-    })
+`;
+        });
+      });
   }
-           
-  }
-
+}
 
 {
   /* <img src="https://64.media.tumblr.com/9901d45b3bf8deffd857dfc5d0e6b677/tumblr_nnp2ze4bIr1r2ll09o1_640.jpg" class="card-img-top " height="400px" alt="..."></img> */
 }
-
-
-
-
