4af61af36cb5720eb56488c375e3c547d0e656e5
Aleksandra Stojanovska <alexandra.stojanovska2101@gmail.com>
2021-10-13 14:33:54 +0200

    ..

--- view-post.js
+++ view-post.js
@@ -4,11 +4,11 @@ let id = url.substring(url.lastIndexOf("=") + 1);
 const renderData = function (post) {
   const html = `
     <div class="card" style="width: 30rem; height: 30rem;">
-    <div class="card-img" style="background-image:url(${post.imageUrl}) ;height:30rem;"></div>
+    <div class="card-img " style="background-image:url(${post.imageUrl}) ;height:30rem; "></div>
     <div class="card-body">
       <h5 class="card-title">${post.title}</h5>
       <p class="card-text" style = "height:5rem;">${post.content}</p>
-      <a href="/edit-post.html?post_id=${post.id}" class="btn btn-design">Edit post</a>
+      <a href="/edit-post.html?post_id=${post.id}" class="btn btn-design    mt-4">Edit post</a>
       
     </div>
   </div>
